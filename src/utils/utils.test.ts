482597edef9d63a9699e93c33fa473c843fdaff0
Romain Lancia <romainlancia@gmail.com>
2022-08-29 19:16:11 +0200

    :truck: chore: setup jest

---
+++ src/utils/utils.test.ts
@@ -0,0 +1,9 @@
+import { formatNationalId, capitalizeFirstLetter } from "."
+
+test('expect "1" to be format as "001"', () => {
+  expect(formatNationalId("1")).toBe("001");
+});
+
+test('expect "test" to be format as "Test"', () => {
+    expect(capitalizeFirstLetter('test'))
+})
